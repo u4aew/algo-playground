42913246f35bf870443cd250fbf5a9c1a4afea45
Sergey Uchaev <s.uchaev@bpcbt.com>
2023-01-01 16:19:13 +0700

    add algo

--- algos/binary-search.js
+++ algos/binary-search.js
@@ -4,20 +4,42 @@ const {array10000} = require('../assets/test-data')
 // Lead time 0.22508399933576584
 const getElementSimple = (num) => {
     const startTime = performance.now()
-    let result = null
     for (let i = 0; i < array10000.length; i++) {
+        // console.log(i)
         if (array10000[i] === num) {
-             result = num
-             break
+             console.log('Lead time', performance.now() - startTime)
+             return num
         }
     }
-    console.log('Lead time', performance.now() - startTime)
-    return result
 }
 
-console.log(getNumberSimple(6952))
+console.log(getElementSimple(6952), 'without algorithm')
 
 // Implementation with algorithm
+// Lead time 0.006750002503395081
 const getElementAlgo = (num) => {
+  const startTime = performance.now()
+  let low = 0
+  let high = array10000.length - 1
+
+  while (low <= high) {
+      console.log(low, 'low')
+      console.log(high, 'high')
+      let mid = Math.floor((high + low) / 2)
+      console.log(mid, 'mid')
+      let guess = array10000[mid]
+      // console.log(guess)
+      if (guess === num ) {
+          console.log('Lead time', performance.now() - startTime)
+          return num
+      }
+      if (guess > num) {
+          high = mid - 1
+      } else {
+          low = mid + 1
+      }
+  }
 
 }
+
+console.log(getElementAlgo(6952), 'with algorithm')
