0b11b48089f6144a5e8e49e6808ebb951d642c7f
Sergey Uchaev <s.uchaev@bpcbt.com>
2023-01-12 11:29:36 +0700

    add sort

---
+++ algos/sort.js
@@ -0,0 +1,25 @@
+const findSmallest = (arr) => {
+    let smallest = arr[0] // 5;
+    let smallestIndex = 0 // 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < smallest) {
+        smallest = arr[i];
+        smallestIndex = i;
+        }
+    }
+    return smallestIndex;
+}
+
+const selectionSort = (arr) => {
+    const newArr = [];
+    const length = arr.length;
+
+    for (let i = 0; i < length; i++) {
+        const smallest = findSmallest(arr);
+        newArr.push(arr.splice(smallest, 1)[0]);
+    }
+    return newArr;
+}
+
+console.log(selectionSort([5, 3, 6, 2, 10]), 'result')
